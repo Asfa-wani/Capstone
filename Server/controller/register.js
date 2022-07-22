2330acccea01c75494b8642adf084ecbf62f9b4f
Asfa Wani <waniasfa@gmail.com>
2022-07-22 10:34:56 +0530

    Created register function

--- Server/controller/register.js
+++ Server/controller/register.js
@@ -1,7 +1,33 @@
-const User = require("../models/user");
+//IMPORTING
+const { User, validate } = require("../models/user");
+const bcrypt = require("bcrypt");
 
-const registerUser = async() => {
+//FUNCTION TO REGISTER/SAVE USERS TO THE DB
+const registerUser = async(req, res) => {
+    try {
+        //VALIDATE THE INPUT DATA
+        const { error } = validate(req.body);
+        if (error)
+            return res.status(401).send({ message: error.details[0].message });
 
+
+        const user = await User.findOne({ email: req.email });
+        if (user)
+            return res.status(409).send({ message: "user with this email already exists!" });
+
+        //GENERATE SALT FOR HASHING THE PASSWORD
+        const salt = await bcrypt.genSalt(Number(process.env.SALT));
+        //ENCRYPT THE PASSWORD
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+        //SAVE THE USER IN THE DB
+        await new User({...req.body, password: hashPassword }).save();
+        return res.status(201).send({ message: "Registered successfully" });
+
+    } catch (error) {
+
+        res.status(500).send({ message: "server error" });
+    }
 };
 
-module.exports = registerUser;
+module.exports = { registerUser };
