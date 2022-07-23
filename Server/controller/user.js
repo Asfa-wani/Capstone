672e9b585e78d0c976904dede8a16031c8f3b202
Asfa Wani <waniasfa@gmail.com>
2022-07-23 16:46:28 +0530

    CREATED USER delete OPERATION

--- Server/controller/user.js
+++ Server/controller/user.js
@@ -9,14 +9,17 @@ const bcrypt = require("bcrypt");
 //FUNCTION TO UPDATE USER INFO
 const updateUser = async(req, res) => {
     try {
+        //EXTRACT DATA FROM REQUEST
         const { name, password, address, phone, age } = req.body;
+        //VALIDATE DATA
         const { error } = validateUpdate({ name, password, address, phone, age });
-
         if (error)
             return res.status(401).send({ message: error.details[0].message });
-
+        //ENCRYPT THE PASSWORD
         const salt = await bcrypt.genSalt(Number(process.env.SALT));
         const hashPassword = await bcrypt.hash(password, salt);
+
+        //UPDATE  USER INFO
         await User.findByIdAndUpdate({ _id: req.params.id }, {
             name: name,
             password: hashPassword,
@@ -32,13 +35,20 @@ const updateUser = async(req, res) => {
     } catch (error) {
         res.status(500).send({ message: "Internal server error" });
     }
-
-
-
 }
 
-module.exports = { updateUser };
+//FUNCTION TO DELETE USER
+const deleteUser = async(req, res) => {
+    try {
+        await User.findByIdAndDelete({ _id: req.params.id });
+        res.status(200).send({ message: "Deleted successfully" });
+    } catch (error) {
+        res.status(500).send({ message: "internal server error" })
+    }
+}
 
+module.exports = { updateUser, deleteUser };
+//VALIDATE INFO FROM USER USING JOI VALIDATION
 const validateUpdate = (data) => {
     const schema = joi.object({
         name: joi.string().required().label("Email"),
--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,13 +1,14 @@
 //IMPORTING
 const router = require("express").Router();
 const { registerUser, loginUser } = require("../controller/auth");
-const { updateUser } = require("../controller/user");
+const { updateUser, deleteUser } = require("../controller/user");
 
 
 //CREATE ROUTES FOR USER LOGIN AND REGISTER
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.put("/update/:id", updateUser);
+router.delete("/delete/:id", deleteUser);
 
 //EXPORT ROUTES
 module.exports = router;
